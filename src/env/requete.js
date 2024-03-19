1de5739ee7daf7d471e2fa2ca219747b56e52635
Adinsi Abdias <lagraceparle98@gmail.com>
2024-03-19 14:52:02 +0100

    push

--- src/env/requete.js
+++ src/env/requete.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const requete = {
-  admin: "http://localhost:7200/v1/admin",
-  // admin: "https://leguidebj-app.onrender.com/v1/admin",
+  // admin: "http://localhost:7200/v1/admin",
+  admin: "https://leguidebj-app.onrender.com/v1/admin",
   // user: "https://leguidebj-app.onrender.com/v1/users",
   // video: "http://localhost:7200/v1/video/preach",
   // comment: "",
